c9e735ce6f518a33df9a5ef74af00b19f4b96d8c
unknown <2002milasari@gmail.com>
2026-01-16 06:32:54 +0700

    fix: update payment logic and file upload

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -11,7 +11,7 @@ const OrderStatus = async ({params}: TPageProps) => {
 
   return (
     <main className="bg-gray-100 min-h-[80vh]">
-      <div className="max-w-5xl mx-auto py-15">
+      <div className="max-w-5xl mx-auto pt-40 pb-10">
         <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
       </div>
       {transaction.status === "pending" && <OrderSubmitted/> }
